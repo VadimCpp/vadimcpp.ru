43f096d2fdaeb811e15c0d0e82c1208a8a61989c
Vadim Cpp <vadim.cpp@gmail.com>
2018-10-17 19:45:39 +0300

    #13

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,20 @@
+var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
-var gulp = require('gulp');
 
-//
-// Configure the browserSync task
-//
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: './'
-    }
-  })
+// https://stackoverflow.com/a/50658518/1775459
+gulp.task('serve', function() {
+
+	browserSync.init({
+	    files: [
+	        {
+	            match: ['*.html'],
+	            fn:    function (event, file) {
+	                this.reload()
+	            }
+	        }
+	    ],
+	    server: './'
+	});
+
+	browserSync.watch('*.html').on('change', browserSync.reload);
 });
-
-
-
-gulp.task('watch', function() {
-    gulp.watch('index.html', browserSync.reload);
-    gulp.watch('css/**/*', browserSync.reload);
-});
-
-
-gulp.task('default', gulp.series('browserSync', 'watch', function() { 
-    // default task code here
-}));
